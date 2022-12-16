d8ad027e1fc0fe1cccca4d3e6b531edb0693fcc9
Matthias Kübrich <matthias.kuebrich@t-online.de>
2022-12-16 17:41:39 +0100

    component.h COMPONENT_SPLITTING check

--- include/uk/component.h
+++ include/uk/component.h
@@ -2,6 +2,10 @@
 
 #include <uk/config.h>
 
+#ifndef CONFIG_COMPONENT_SPLITTING
+#error "COMPONENT_SPLITTING must be enabled"
+#endif
+
 #define __UK_LIB_COMPONENT(lib) __##lib##_COMPONENT__
 #define UK_LIB_COMPONENT(lib) __UK_LIB_COMPONENT(lib)
 
