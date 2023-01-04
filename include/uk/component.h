15e8fb35b7c17176fe511bb7623cb4a9363a81fb
Matthias Kübrich <matthias.kuebrich@t-online.de>
2023-01-04 15:38:41 +0100

    macros for stating component sections

--- include/uk/component.h
+++ include/uk/component.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <uk/config.h>
+#include <uk/essentials.h>
 
 #ifndef CONFIG_COMPONENT_SPLITTING
 #error "COMPONENT_SPLITTING must be enabled"
@@ -10,3 +11,10 @@
 #define UK_LIB_COMPONENT(lib) __UK_LIB_COMPONENT(lib)
 
 #define UK_COMPONENT UK_LIB_COMPONENT(__LIBNAME__)
+
+#define UK_LIB_SECTION(lib, section)                                           \
+	__section(".component" STRINGIFY(UK_LIB_COMPONENT(lib)) "." STRINGIFY( \
+	    section))
+
+#define UK_SHARE_SECTION(share, section)                                       \
+	__section("." STRINGIFY(share) "." STRINGIFY(section))
