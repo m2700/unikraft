b7c19c0ac1ccf727c975abfad15426868f5908e0
Matthias Kübrich <matthias.kuebrich@t-online.de>
2023-01-13 13:31:00 +0100

    refactured trampoline-share

--- include/uk/trampoline-share.h
+++ include/uk/trampoline-share.h
@@ -1,29 +1,29 @@
 #include <uk/config.h>
 
-#ifdef __HEADER_LIBNAME__
+#ifdef __SRC_LIBNAME__
 
-#ifdef UK_HEADER_COMPONENT
-#undef UK_HEADER_COMPONENT
+#ifdef UK_SRC_COMPONENT
+#undef UK_SRC_COMPONENT
 #endif
 
 #ifdef UK_TRAMPOLINE_SHARE
 #undef UK_TRAMPOLINE_SHARE
 #endif
 
-#define UK_HEADER_COMPONENT UK_LIB_COMPONENT(__HEADER_LIBNAME__)
+#define UK_SRC_COMPONENT UK_LIB_COMPONENT(__SRC_LIBNAME__)
 
 #if !defined(CONFIG_COMPONENT_SPLITTING)
 // no shares defined without component splitting
-#elif UK_HEADER_COMPONENT < UK_COMPONENT
+#elif UK_SRC_COMPONENT < UK_COMPONENT
 #define UK_TRAMPOLINE_SHARE                                                    \
-	UK_CONCAT(UK_CONCAT(component, UK_HEADER_COMPONENT),                   \
+	UK_CONCAT(UK_CONCAT(component, UK_SRC_COMPONENT),                      \
 		  UK_CONCAT(_, UK_COMPONENT))
-#elif UK_HEADER_COMPONENT > UK_COMPONENT
+#elif UK_SRC_COMPONENT > UK_COMPONENT
 #define UK_TRAMPOLINE_SHARE                                                    \
 	UK_CONCAT(UK_CONCAT(component, UK_COMPONENT),                          \
-		  UK_CONCAT(_, UK_HEADER_COMPONENT))
+		  UK_CONCAT(_, UK_SRC_COMPONENT))
 #else
 #define UK_TRAMPOLINE_SHARE UK_CONCAT(component, UK_COMPONENT)
 #endif
 
-#endif // __HEADER_LIBNAME__
+#endif // __SRC_LIBNAME__
