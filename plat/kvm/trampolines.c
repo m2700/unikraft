91c2407588b15c704b101a0a82765e211b801252
Matthias Kübrich <matthias.kuebrich@t-online.de>
2023-03-30 16:39:56 +0200

    trampoline category comments

--- plat/kvm/trampolines.c
+++ plat/kvm/trampolines.c
@@ -1,6 +1,9 @@
 #include <uk/auto-trampoline.h>
 
+// shutdown.c
 AUTO_TRAMPOLINE(ukplat_terminate)
+
+// console
 AUTO_TRAMPOLINE(ukplat_coutd)
 AUTO_TRAMPOLINE(ukplat_coutk)
 AUTO_TRAMPOLINE(ukplat_cink)
