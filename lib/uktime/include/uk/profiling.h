ed26c12d535aaeed368a8c67ed5f267bcc6a8e09
Matthias Kübrich <matthias.kuebrich@t-online.de>
2023-03-30 20:28:09 +0200

    fix prof id usage

--- lib/uktime/include/uk/profiling.h
+++ lib/uktime/include/uk/profiling.h
@@ -18,12 +18,12 @@ extern __sz uk_prf_id_count;
 	UK_COMP_PUBLIC_SECTION(".", "bss")                                     \
 	static __sz __uk_prf_id_##name = 0;                                    \
 	__uk_prf_id_##name = __uk_prf_id_##name || ++uk_prf_id_count;          \
-	uk_prf_counts[__uk_prf_id_##name]++;                                   \
-	uk_prf_names[__uk_prf_id_##name] = #name;                              \
+	uk_prf_counts[__uk_prf_id_##name - 1]++;                               \
+	uk_prf_names[__uk_prf_id_##name - 1] = #name;                          \
 	__nsec __uk_prf_ts_##name = ukplat_monotonic_clock()
 
 #define UK_PRF_END(name)                                                       \
-	uk_prf_delays[__uk_prf_id_##name] +=                                   \
+	uk_prf_delays[__uk_prf_id_##name - 1] +=                               \
 	    ukplat_monotonic_clock() - __uk_prf_ts_##name
 
 #else // CONFIG_LIBUKTIME_PROFILING
