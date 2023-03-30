1bf3ebd9243c5831e9607f715754bc44a45a6201
Matthias Kübrich <matthias.kuebrich@t-online.de>
2023-03-30 20:27:49 +0200

    + dyn trampoline

--- lib/uktime/profiling.c
+++ lib/uktime/profiling.c
@@ -5,6 +5,7 @@
 #include <uk/component.h>
 #include <uk/print.h>
 #include <stdio.h>
+#include <uk/dyn-trampoline.h>
 
 UK_COMP_PUBLIC_SECTION(".", "bss")
 char const *uk_prf_names[CONFIG_LIBUKTIME_PROFILING_ARRAY_SIZE];
@@ -21,8 +22,10 @@ __sz uk_prf_id_count = 0;
 UK_COMP_PUBLIC_SECTION(".", "text")
 void print_profiling_results()
 {
+	DYN_TRAMPOLINE_INIT;
 	for (__sz i = 0; i < uk_prf_id_count; i++) {
 		printf("Profiling: %s: %llu ns, %llu times\n", uk_prf_names[i],
 		       uk_prf_delays[i], uk_prf_counts[i]);
 	}
+	DYN_TRAMPOLINE_FINI;
 }
