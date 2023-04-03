98d65755324ada0684c0fce40b553d6880f11fe2
Matthias Kübrich <matthias.kuebrich@t-online.de>
2023-04-03 23:30:28 +0200

    single profiling events

--- lib/uktime/include/uk/profiling.h
+++ lib/uktime/include/uk/profiling.h
@@ -23,18 +23,24 @@ extern __u64 uk_prf_tsc_delays[CONFIG_LIBUKTIME_PROFILING_ARRAY_SIZE];
 	uk_prf_tsc_delays[__uk_prf_id_##name - 1] +=                           \
 	    rdtsc_ordered() - __uk_prf_tsc_##name
 
-#define UK_PRF_START(name)                                                     \
+#define __LOCAL_PRF_ID(name) __sz __uk_prf_id_##name = ++uk_prf_id_count
+#define __STATIC_PRF_ID(name)                                                  \
 	UK_COMP_PUBLIC_SECTION(".", "bss")                                     \
 	static __sz __uk_prf_id_##name = 0;                                    \
 	if (__uk_prf_id_##name == 0) {                                         \
 		__uk_prf_id_##name = ++uk_prf_id_count;                        \
-	}                                                                      \
+	}
+
+#define __UK_PRF_START(name, prf_id)                                           \
+	prf_id(name);                                                          \
 	uk_prf_counts[__uk_prf_id_##name - 1]++;                               \
 	uk_prf_names[__uk_prf_id_##name - 1] = #name;                          \
 	__MEASURE_TSC(name)
 
-#define UK_PRF_END(name)                                                       \
-	__SAVE_TSC(name);                                                      \
+#define UK_PRF_START(name) __UK_PRF_START(name, __STATIC_PRF_ID)
+#define UK_PRF_START_SINGLE(name) __UK_PRF_START(name, __LOCAL_PRF_ID)
+
+#define UK_PRF_END(name) __SAVE_TSC(name);
 
 #else // CONFIG_LIBUKTIME_PROFILING
 
